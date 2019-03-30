5646afb359a8f37a9c394353b5be8298f22f71c8
Sanga <jamessanga6@gmail.com>
2019-03-30 12:35:21 +0300

    Favorite adapter was added

--- app/src/main/java/com/tz/sanga/moviestore/Adapters/FavoriteAdapter.java
+++ app/src/main/java/com/tz/sanga/moviestore/Adapters/FavoriteAdapter.java
@@ -1,6 +1,9 @@
 package com.tz.sanga.moviestore.Adapters;
 
+import android.app.Activity;
 import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,55 +11,71 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.GlideDrawable;
+import com.bumptech.glide.request.RequestListener;
+import com.bumptech.glide.request.target.Target;
 import com.tz.sanga.moviestore.Database.MovieObjects;
 import com.tz.sanga.moviestore.R;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
-public class FavoriteAdapter extends ArrayAdapter {
+public class FavoriteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+    private static final String BASE_URL_IMG = "https://image.tmdb.org/t/p/original";
+    private Context context;
+    List<MovieObjects>movieObjects= Collections.emptyList();
 
-    List list= new ArrayList();
-    public FavoriteAdapter(Context context, int resource){
-        super(context,resource);
-    }
-    static class LayoutHandler{
-        TextView textView;
-        ImageView imageView;
-        String path;
-    }
-    public void add(Object object){
-        super.add(object);
-        list.add(object);
-
-    }
-    public int getCount(){
-        return list.size();
-    }
-    public Object getTtem(int position){
-        return list.get(position);
+    public FavoriteAdapter(Context context, List<MovieObjects> movieObjects) {
+        this.context = context;
+        this.movieObjects = movieObjects;
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        View row = convertView;
-        LayoutHandler layoutHandler;
-        if(row == null)
-        {
-            LayoutInflater layoutInflater =(LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            row=layoutInflater.inflate(R.layout.display_view,parent,false);
-            layoutHandler=new LayoutHandler();
-            layoutHandler.textView=row.findViewById(R.id.view_text);
-            layoutHandler.imageView = row.findViewById(R.id.img_view);
-            row.setTag(layoutHandler);
-        }
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.display_view, viewGroup, false);
+        MyHolder myHolder = new MyHolder(view);
+        return myHolder;
+    }
 
-        else{
-            layoutHandler=(LayoutHandler)row.getTag();
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder viewHolder, int i) {
+        MyHolder myHolder = (MyHolder)viewHolder;
+        myHolder.textView.setText(movieObjects.get(i).getTitle());
+
+        Glide.with(context)
+                .load(BASE_URL_IMG+movieObjects.get(i).getPath())
+                .listener(new RequestListener<String, GlideDrawable>() {
+                    @Override
+                    public boolean onException(Exception e, String model, Target<GlideDrawable> target, boolean isFirstResource) {
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(GlideDrawable resource, String model, Target<GlideDrawable> target, boolean isFromMemoryCache, boolean isFirstResource) {
+                        return false;
+                    }
+                })
+                .centerCrop()
+                .into(myHolder.imageView);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return movieObjects.size();
+    }
+
+    class MyHolder extends RecyclerView.ViewHolder{
+        private TextView textView;
+        private ImageView imageView;
+
+        public MyHolder(@NonNull View itemView) {
+            super(itemView);
+            textView = itemView.findViewById(R.id.view_text);
+            imageView = itemView.findViewById(R.id.img_view);
         }
-        MovieObjects movieObjects=(MovieObjects) this.getTtem(position);
-        layoutHandler.textView.setText(movieObjects.getTitle());
-        return row;
     }
 }
 
