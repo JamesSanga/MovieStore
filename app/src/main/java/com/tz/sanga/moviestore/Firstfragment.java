3e979cef1ac5da480245a3181cfe10ae881eb4d5
Sanga <jamessanga6@gmail.com>
2019-04-01 21:05:03 +0300

    Navigation architecture components implementation

---
+++ app/src/main/java/com/tz/sanga/moviestore/Firstfragment.java
@@ -0,0 +1,29 @@
+package com.tz.sanga.moviestore;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Firstfragment extends Fragment {
+
+
+    public Firstfragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_firstfragment, container, false);
+    }
+
+}
