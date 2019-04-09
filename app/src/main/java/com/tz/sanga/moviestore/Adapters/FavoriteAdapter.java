0deb78b1d84218fbd3df8cbf0e72fe063a8e8672
Sanga <jamessanga6@gmail.com>
2019-04-09 10:10:10 +0300

    Arranging work in good way

--- app/src/main/java/com/tz/sanga/moviestore/Adapters/FavoriteAdapter.java
+++ app/src/main/java/com/tz/sanga/moviestore/Adapters/FavoriteAdapter.java
@@ -1,13 +1,11 @@
 package com.tz.sanga.moviestore.Adapters;
 
-import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -16,10 +14,9 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.drawable.GlideDrawable;
 import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.target.Target;
-import com.tz.sanga.moviestore.Database.MovieObjects;
+import com.tz.sanga.moviestore.Model.MovieObjects;
 import com.tz.sanga.moviestore.R;
 
-import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
