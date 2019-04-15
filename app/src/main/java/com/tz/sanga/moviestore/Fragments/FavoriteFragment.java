da7497c598676f93789e98c9bf2ac64912998e89
Sanga <jamessanga6@gmail.com>
2019-04-15 17:34:04 +0300

    New fragment to view favorite movies separate from movie details fragment

---
+++ app/src/main/java/com/tz/sanga/moviestore/Fragments/FavoriteFragment.java
@@ -0,0 +1,103 @@
+package com.tz.sanga.moviestore.Fragments;
+
+
+import android.database.Cursor;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.tz.sanga.moviestore.Activities.MainActivity;
+import com.tz.sanga.moviestore.Adapters.FavoriteAdapter;
+import com.tz.sanga.moviestore.Model.Favorite;
+import com.tz.sanga.moviestore.Model.FavoriteDb;
+import com.tz.sanga.moviestore.Model.MovieObjects;
+import com.tz.sanga.moviestore.R;
+
+import java.util.ArrayList;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static android.support.constraint.Constraints.TAG;
+
+
+public class FavoriteFragment extends Fragment {
+
+    @BindView(R.id.recycler_view) RecyclerView recyclerView;
+   // @BindView(R.id.gride_view) GridView gridView;
+
+    private FavoriteDb favoriteDb;
+    private ActionBar actionBar;
+    private ArrayList<MovieObjects> movieList = new ArrayList<>();
+    LinearLayoutManager layoutManager;
+    FavoriteAdapter favoriteAdapter;
+
+    public FavoriteFragment() {
+        // Required empty public constructor
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_favorite, container, false);
+        ButterKnife.bind(this, view);
+        setToolBar();
+        loadSqliteData();
+        return view;
+    }
+
+    private void setToolBar() {
+        actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle("Favorite Movies");
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    public void loadSqliteData(){
+        // favoritePresenter.loadSqlLiteData();
+        favoriteDb = new FavoriteDb(getContext());
+        Cursor data = favoriteDb.getMovies("select * from " + Favorite.FavoriteEntry.TABLE_NAME);
+        Log.d(TAG, "loadSqliteData:sql data " +data.getCount());
+
+            if (data.moveToNext()) {
+                do {
+                    MovieObjects movieObjects = new MovieObjects();
+                    movieObjects.setTitle(data.getString(2));
+                    movieObjects.setPath(data.getString(4));
+                    movieList.add(movieObjects);
+
+                }while (data.moveToNext());
+            }
+
+           layoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+           recyclerView.setLayoutManager(layoutManager);
+           favoriteAdapter = new FavoriteAdapter(getContext(), movieList);
+           recyclerView.setItemAnimator(new DefaultItemAnimator());
+       // gridView.setAdapter((ListAdapter) favoriteAdapter);
+    }
+}
