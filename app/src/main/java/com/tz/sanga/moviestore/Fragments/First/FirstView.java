8cbf18ad96bfe449f9abae209160ce8bf53ee1f7
Sanga <jamessanga6@gmail.com>
2019-04-16 10:33:49 +0300

    trailer movies results interface method that trigger the changes in first fragment where trailer appears

--- app/src/main/java/com/tz/sanga/moviestore/Fragments/First/FirstView.java
+++ app/src/main/java/com/tz/sanga/moviestore/Fragments/First/FirstView.java
@@ -2,6 +2,7 @@ package com.tz.sanga.moviestore.Fragments.First;
 
 import com.tz.sanga.moviestore.Model.Movie;
 import com.tz.sanga.moviestore.Model.MoviesResponse;
+import com.tz.sanga.moviestore.Model.Trailer;
 
 import java.util.List;
 
@@ -10,4 +11,5 @@ public interface FirstView {
     void hideLoading();
     void showResults(List<Movie> moveData);
     void onErrorLoading(String message);
+    void trailerMoviesResults(List<Trailer>trailers);
 }
