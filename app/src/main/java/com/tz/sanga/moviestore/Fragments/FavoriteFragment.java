2d01f369620032c89d9b85e3ba50e63098369a15
Sanga <jamessanga6@gmail.com>
2019-04-16 15:25:45 +0300

    Setting up the function of menu to display, adding swipe to refresh favorite movies and implementing method to delete all favorite movies from the SQlite database

--- app/src/main/java/com/tz/sanga/moviestore/Fragments/FavoriteFragment.java
+++ app/src/main/java/com/tz/sanga/moviestore/Fragments/FavoriteFragment.java
@@ -2,17 +2,22 @@ package com.tz.sanga.moviestore.Fragments;
 
 
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tz.sanga.moviestore.Activities.MainActivity;
 import com.tz.sanga.moviestore.Adapters.FavoriteAdapter;
@@ -29,6 +34,7 @@ import butterknife.ButterKnife;
 
 public class FavoriteFragment extends Fragment {
 
+    @BindView(R.id.refresh_favorite_movies) SwipeRefreshLayout refreshLayout;
     @BindView(R.id.recycler_view) RecyclerView recyclerView;
     @BindView(R.id.empty_view)TextView textView;
 
@@ -49,6 +55,7 @@ public class FavoriteFragment extends Fragment {
         ButterKnife.bind(this, view);
         setToolBar();
         loadSqliteData();
+        refreshFavorite();
         return view;
     }
 
@@ -58,15 +65,43 @@ public class FavoriteFragment extends Fragment {
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Favorite Movies");
     }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.delete_favorite, menu);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch(item.getItemId()) {
             case android.R.id.home:
                 getActivity().onBackPressed();
                 return true;
+            case R.id.action_delete:
+                clearFavorite();
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void refreshFavorite(){
+        refreshLayout.setColorScheme(R.color.colorAccent, R.color.lightGray, R.color.black);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                movieList.clear();
+                loadSqliteData();
+            }
+        });
+    }
+
+    private void clearFavorite(){
+        FavoriteDb favoriteDb = new FavoriteDb(getContext());
+        SQLiteDatabase db = favoriteDb.getWritableDatabase();
+        int deleted = db.delete(Favorite.FavoriteEntry.TABLE_NAME, "1", null);
+        if (deleted > 0){
+            Toast.makeText(getContext(), "Favorite Cleared", Toast.LENGTH_LONG).show();
+        }loadSqliteData();
     }
 
     public void loadSqliteData(){
@@ -76,6 +111,8 @@ public class FavoriteFragment extends Fragment {
 
         if (data.getCount() < 1){
             textView.setVisibility(View.VISIBLE);
+            movieList.clear();
+            favoriteAdapter = new FavoriteAdapter(getContext(), movieList);
         }else {
                 textView.setVisibility(View.GONE);
             if (data.moveToNext()) {
@@ -93,5 +130,6 @@ public class FavoriteFragment extends Fragment {
         favoriteAdapter = new FavoriteAdapter(getContext(), movieList);
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.setAdapter(favoriteAdapter);
+        refreshLayout.setRefreshing(false);
     }
 }
