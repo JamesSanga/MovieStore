c7465151a393dfce424fb9fd957d05da75215531
Sanga <jamessanga6@gmail.com>
2019-04-16 10:31:59 +0300

    Implementing movie trailers to mvp

--- app/src/main/java/com/tz/sanga/moviestore/Fragments/First/FirstPresenter.java
+++ app/src/main/java/com/tz/sanga/moviestore/Fragments/First/FirstPresenter.java
@@ -1,21 +1,14 @@
 package com.tz.sanga.moviestore.Fragments.First;
 
-import android.content.Context;
-import android.database.Cursor;
-import android.os.Bundle;
-import android.view.View;
 
 import com.tz.sanga.moviestore.BuildConfig;
-import com.tz.sanga.moviestore.Fragments.FirstFragment;
 import com.tz.sanga.moviestore.Model.API.Connector;
 import com.tz.sanga.moviestore.Model.API.Service;
-import com.tz.sanga.moviestore.Model.Favorite;
-import com.tz.sanga.moviestore.Model.FavoriteDb;
 import com.tz.sanga.moviestore.Model.Movie;
-import com.tz.sanga.moviestore.Model.MovieObjects;
 import com.tz.sanga.moviestore.Model.MoviesResponse;
+import com.tz.sanga.moviestore.Model.Trailer;
+import com.tz.sanga.moviestore.Model.TrailerResponse;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -25,7 +18,7 @@ import retrofit2.Response;
 public class FirstPresenter {
     private static final int PAGE_START = 1;
     private int currentPage = PAGE_START;
-    FirstView firstView;
+    private FirstView firstView;
     private Service service;
     private int moveId;
 
@@ -40,22 +33,17 @@ public class FirstPresenter {
 
     public void getData(){
         firstView.showLoading();
-
         service = Connector.getConnector().create(Service.class);
        callSimilarMoviesApi().enqueue(new Callback<MoviesResponse>() {
            @Override
            public void onResponse(Call<MoviesResponse> call, Response<MoviesResponse> response) {
-               List<Movie> results = fetchResults(response);
+               List<Movie>results = fetchResults(response);
                firstView.showResults(results);
            }
 
            @Override
-           public void onFailure(Call<MoviesResponse> call, Throwable t) {
-
-           }
+           public void onFailure(Call<MoviesResponse> call, Throwable t) {firstView.onErrorLoading(t.getLocalizedMessage());}
        });
-
-
     }
 
     private List<Movie> fetchResults(Response<MoviesResponse> response){
@@ -69,5 +57,22 @@ public class FirstPresenter {
                 currentPage
 
         );
+    }
+
+    public void processTrailer(){
+        Service service = Connector.getConnector().create(Service.class);
+        Call<TrailerResponse> call = service.getTrailer(moveId, BuildConfig.THE_MOVIE_DB_API_TOKEN);
+        call.enqueue(new Callback<TrailerResponse>() {
+            @Override
+            public void onResponse(Call<TrailerResponse> call, Response<TrailerResponse> response) {
+                List<Trailer> trailerList = response.body().getResults();
+                firstView.trailerMoviesResults(trailerList);
+            }
+
+            @Override
+            public void onFailure(Call<TrailerResponse> call, Throwable t) {
+                firstView.onErrorLoading(t.getLocalizedMessage());
+            }
+        });
     }
 }
