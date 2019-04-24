aa13d72037f7bb22fb77ab45d21103b6cc7c922a
Sanga <jamessanga6@gmail.com>
2019-04-24 17:27:25 +0300

    Youtube API key that helps to load videos in application

---
+++ app/src/main/java/com/tz/sanga/moviestore/YoutubeConfig.java
@@ -0,0 +1,12 @@
+package com.tz.sanga.moviestore;
+
+public class YoutubeConfig {
+    public YoutubeConfig(){
+
+    }
+    private static final String API_KEY = "AIzaSyBANlmk5xL8jCTgLXejGC1ipqSDl7Ir-BM";
+
+    public static String getApiKey() {
+        return API_KEY;
+    }
+}
