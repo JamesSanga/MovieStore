e9b9ee7f41b227ecc767ab922a935f57827be20d
Sanga <jamessanga6@gmail.com>
2019-05-02 13:31:15 +0300

    Removing warnings

--- app/src/main/java/com/tz/sanga/moviestore/Fragments/FavoriteFragment.java
+++ app/src/main/java/com/tz/sanga/moviestore/Fragments/FavoriteFragment.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -66,7 +67,7 @@ public class FavoriteFragment extends Fragment implements FavoriteAdapter.dataLi
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_favorite, container, false);
