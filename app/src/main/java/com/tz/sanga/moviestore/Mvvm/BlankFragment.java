011a4254ed9e063faaf3494d3b1d8a67941dee1f
Sanga <jamessanga6@gmail.com>
2019-05-02 13:28:41 +0300

    Model, view, viewModel fragment

---
+++ app/src/main/java/com/tz/sanga/moviestore/Mvvm/BlankFragment.java
@@ -0,0 +1,65 @@
+package com.tz.sanga.moviestore.Mvvm;
+
+
+import android.arch.lifecycle.Observer;
+import android.arch.lifecycle.ViewModelProviders;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.ActionBar;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.tz.sanga.moviestore.Activities.MainActivity;
+import com.tz.sanga.moviestore.R;
+
+import java.util.List;
+
+public class BlankFragment extends Fragment {
+
+    public BlankFragment() {
+        // Required empty public constructor
+        setHasOptionsMenu(true);
+    }
+
+
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_blank, container, false);
+        setToolBar();
+        createViewModel();
+        return view;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home){
+            getActivity().onBackPressed();
+            return true;
+        }else
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setToolBar() {
+        ActionBar actionBar = ((MainActivity) getActivity()).getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setTitle(R.string.mvvp);
+    }
+
+    private void createViewModel(){
+        NoteViewModel noteViewModel = ViewModelProviders.of(this).get(NoteViewModel.class);
+        noteViewModel.getAllNote().observe(this, new Observer<List<Note>>() {
+            @Override
+            public void onChanged(@Nullable List<Note> notes) {
+                Toast.makeText(getContext(), "OnChanged", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+}
