2206437dddb51788c596a709f5a7b0968b88ac79
Sanga <jamessanga6@gmail.com>
2019-05-15 11:03:35 +0300

    Local database and its entities are created here

---
+++ app/src/main/java/com/tz/sanga/moviestore/Model/FavoriteDatabase.java
@@ -0,0 +1,33 @@
+package com.tz.sanga.moviestore.Model;
+
+import android.arch.persistence.db.SupportSQLiteDatabase;
+import android.arch.persistence.room.Database;
+import android.arch.persistence.room.Room;
+import android.arch.persistence.room.RoomDatabase;
+import android.content.Context;
+import android.support.annotation.NonNull;
+
+@Database(entities = {FavoriteNote.class}, version = 1)
+public abstract class FavoriteDatabase extends RoomDatabase {
+
+    private static FavoriteDatabase instance;
+    private static RoomDatabase.Callback roomDatabase = new RoomDatabase.Callback(){
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+        }
+    };
+
+    public static synchronized FavoriteDatabase getInstance(Context context){
+        if (instance == null){
+            instance = Room.databaseBuilder(context.getApplicationContext(), FavoriteDatabase.class, "favorite")
+                    .fallbackToDestructiveMigration()
+                    .addCallback(roomDatabase)
+                    .build();
+        }
+        return instance;
+    }
+
+    public abstract FavoriteDao favoriteDao();
+
+}
