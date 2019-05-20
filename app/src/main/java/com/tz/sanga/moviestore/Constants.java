f0a35e56546ba0cb01688e9443b45ef1a7975e02
Sanga <jamessanga6@gmail.com>
2019-05-20 11:29:04 +0300

    Three constants added (path, FavoriteDatabase and Repository)

--- app/src/main/java/com/tz/sanga/moviestore/Constants.java
+++ app/src/main/java/com/tz/sanga/moviestore/Constants.java
@@ -1,5 +1,8 @@
 package com.tz.sanga.moviestore;
 
+import com.tz.sanga.moviestore.Database.Local.FavoriteDatabase;
+import com.tz.sanga.moviestore.Repositories.Repository;
+
 public class Constants {
     public Constants() {}
 
@@ -8,4 +11,8 @@ public class Constants {
     public static String getImageUrl() {
         return BASE_URL_IMG;
     }
+
+    public static String path = "/or06FN3Dka5tukK1e9sl16pB3iy.jpg";
+    public static FavoriteDatabase favoriteRoomDatabase;
+    public static Repository repository;
 }
