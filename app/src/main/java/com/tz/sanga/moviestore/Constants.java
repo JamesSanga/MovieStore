69b5cf81c99f4cc7f66993e031ba83e07506e20c
Sanga <jamessanga6@gmail.com>
2019-05-20 18:17:18 +0300

    Removing constant path in constants

--- app/src/main/java/com/tz/sanga/moviestore/Constants.java
+++ app/src/main/java/com/tz/sanga/moviestore/Constants.java
@@ -1,16 +1,13 @@
 package com.tz.sanga.moviestore;
 
-import com.tz.sanga.moviestore.Database.Local.FavoriteDatabase;
+import com.tz.sanga.moviestore.Repositories.FavoriteRepository;
 
 public class Constants {
     public Constants() {}
 
     private static final String BASE_URL_IMG = "https://image.tmdb.org/t/p/original";
-
     public static String getImageUrl() {
         return BASE_URL_IMG;
     }
-
-    public static String path = "/or06FN3Dka5tukK1e9sl16pB3iy.jpg";
-    public static FavoriteDatabase favoriteRoomDatabase;
+    public static FavoriteRepository favoriteRepository;
 }
